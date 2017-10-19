df16320f07efd7988881ca60ae394ec85a770efb
Talant Ruzmetov <truzmeto@kent.edu>
2017-10-19 17:25:43 -0400

    done

--- distinct_config.R
+++ distinct_config.R
@@ -1,7 +1,8 @@
 #!/bin/Rscript
 
 # read data
-data <- read.table("tmp", sep="", header = FALSE)
+input_file <- "./tmp"
+data <- read.table(input_file, sep="", header = FALSE)
 
 #split data frame based on factor into many d.frames(V1 is n_esc)
 dframes <- split(data, as.factor(data$V1))
@@ -12,8 +13,8 @@ my_list <- vector("list", 12)
 # number of dframes in a list
 N <- length(dframes)
 
-uniq_frame <- 0
-dq_thresh <- 3
+dq_thresh <- 3 # threshold in q to distinguish configurations
+uniq <- 0 
 
 #loop over list elements(data.frames)
 for(i in 1:(N-1)){
@@ -26,15 +27,26 @@ for(i in 1:(N-1)){
     #count total number of configs(frames) for each d.frame  
     colN <- ncol(my_list[[i]])
     
+    # loop over columns, compare each column to all other columns and subset
+    # the matrix based on difference. This helps to count # of uniq configurations
+    k <- 0  
     for(j in 1:colN){    
-        a <- colSums(abs(my_list[[i]] - my_list[[i]][,1])) > dq_thresh
-        my_list[i] <- my_list[[i]][,a]
-        uniq[i] <- uniq[i] + 1      
+        # generating col.indicies for uniq columns
+        sub_indx <- colSums(abs(my_list[[i]] - my_list[[i]][,1])) > dq_thresh
+  
+        # subsetting matrix based on uniq index
+        my_list[[i]] <- my_list[[i]][,sub_indx]
         
-        a <- 0
+        # counting # of uniq cases
+        k <- k + 1      
+        
+        # setting sub index to zero
+        sub_indx <- 0
+        
+        # here I break out if matrix collapses to one column or less
         if(ncol(my_list[[i]]) <= 1) break
     }
+    uniq[i] <- k 
 }
 
-#      uniq[j] <- sum(unique(colSums(abs(dframes[[i]] - dframes[[i]][,j]))) > 2) # too nested
-#uniq_frame[i] <- sum(uniq)
+print(uniq)
